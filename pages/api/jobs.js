0c8b90f665134cefed616a1e8af8ee2a81bcf9f6
Tanmay Patange <tanmay.patange@gmail.com>
2025-02-21 23:18:05 +0530

    Update jobs.js

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      'https://api.ashbyhq.com/posting-api/job-board/openai?includeCompensation=false',
+      'https://api.ashbyhq.com/posting-api/job-board/openai?includeCompensation=true',
       {
         method: 'GET',
         headers: {
